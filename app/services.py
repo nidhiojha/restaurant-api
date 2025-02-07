b53df5a08c38272672afca6d39712446647eb28f
107 NIDHI OJHA NIT PATNA <ojhanidhi2005@gmail.com>
2025-02-07 22:36:40 +0530

    add gitignore

--- app/services.py
+++ app/services.py
@@ -1,22 +1,18 @@
 from geopy.distance import geodesic
 from datetime import datetime
-from app.utils import load_data
+from app.utils import load_restaurant_data
 
-def get_available_restaurants(latitude: float, longitude: float):
-    data = load_data()
-    print("data type........", type(data))
-    # OR
-    data.fillna({'latitude': 0.0, 'longitude': 0.0}, inplace=True)  # Replace NaN with default values
+def fetch_valid_restaurants(latitude: float, longitude: float):
+    restaurant_data = load_restaurant_data()
+    restaurant_data.fillna({'latitude': 0.0, 'longitude': 0.0}, inplace=True)
 
     user_location = (latitude, longitude)
     now = datetime.utcnow().time()
-    print("lat..:", data["latitude"])
-    print("long..:", data["longitude"])
 
     valid_restaurants = []
-    for _, row in data.iterrows():
-        store_location = (row['latitude'], row['longitude'])
-        if geodesic(user_location, store_location).km <= row['availability_radius']:
+    for _, row in restaurant_data.iterrows():
+        store_loc = (row['latitude'], row['longitude'])
+        if geodesic(user_location, store_loc).km <= row['availability_radius']:
             open_time = datetime.strptime(row['open_hour'], "%H:%M:%S").time()
             close_time = datetime.strptime(row['close_hour'], "%H:%M:%S").time()
             if open_time <= now <= close_time:
